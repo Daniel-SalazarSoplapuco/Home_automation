72af997f7440b31801b443c7276e6484d188efd6
Daniel <d.fsalazarsoplapuco@gmail.com>
2021-08-29 23:04:00 +0100

    progress on v3

---
+++ Home_security/V3/Radar.py
@@ -0,0 +1,61 @@
+import datetime as dt
+import time as tm
+import RPi.GPIO as GPIO
+from gpiozero import MotionSensor
+from queue import Queue
+import threading
+
+
+class Radar:
+
+    def __init__(self, queue_object :object = None, delay: int = 2, motion_pin: int = 20, stop_event: object = object):
+        
+        if queue_object is None:
+            self.queue_object = print
+        else:
+            self.queue_object = queue_object.put
+        
+        self.delay = delay
+        self.radar_object = MotionSensor(motion_pin)
+        self.stop_event = stop_event
+
+
+    def run(self):
+        while:
+            self.radar_object.wait_for_inactive()
+            Thread = "Radar"
+            Message = "[Radar]: Motion detected at [{}]".format(dt.datetime.now())
+            Arguments = [True, dt.datetime.now().strftime("%Y%m%d %H:%M:%S")]
+            self.queue_object([Thread, Message, Arguments])
+            tm.sleep(self.delay)
+
+            if self.stop_event.is_set():
+                self.clean_up
+                break
+
+    def clean_up(self):
+        GPIO.cleanup()
+
+    def terminate(self):
+        self.clean_up()
+        self.stop = True   
+
+if __name__ == '__main__':
+    
+    queue_object = Queue(maxsize=100)
+    stop_event= threading.Event()
+    process = Radar(stop_event=stop_event, queue_object=queue_object)
+
+    radar_thread = threading.Thread(target=process.run)
+    radar_thread.daemon = True
+    radar_thread.start()
+    
+    for x in range(0, 10):
+        item = queue_object.get()
+        print(item)
+        queue_object.task_done()
+        tm.sleep(1)
+
+    print("End program")
+    stop_event.set()
+    
